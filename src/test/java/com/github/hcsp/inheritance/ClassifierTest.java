e47bd37ff7c8573380f9ea5b975c4739765bfbf2
Bo Zhang <bo@gradle.com>
2019-06-22 20:49:19 +0800

    Remove bad tests

--- src/test/java/com/github/hcsp/inheritance/ClassifierTest.java
+++
@@ -1,9 +0,0 @@
-package com.github.hcsp.inheritance;
-
-import org.junit.jupiter.api.Assertions;
-
-public class ClassifierTest {
-    public void test() {
-        Assertions.assertTrue(Base.class, Sub.class.getSuperclass());
-    }
-}
