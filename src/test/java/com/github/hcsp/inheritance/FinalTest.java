3cf54c43e5866bfd5f12962e09559d21f13ecfc1
Bo Zhang <bo@gradle.com>
2019-06-30 17:08:00 +0800

    Correct test

--- src/test/java/com/github/hcsp/inheritance/FinalTest.java
+++ src/test/java/com/github/hcsp/inheritance/FinalTest.java
@@ -1,8 +1,10 @@
 package com.github.hcsp.inheritance;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class FinalTest {
+    @Test
     public void test() {
         Assertions.assertEquals(Base.class, Sub.class.getSuperclass());
     }
