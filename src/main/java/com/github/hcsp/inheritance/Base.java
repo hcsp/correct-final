81a0bc85bf529bb7ce193e9440b62ad9353a2781
Felix Mi <41504852+mm123mm123@users.noreply.github.com>
2020-01-11 15:25:49 +0800

    消除final关键字 (#106)
    消除final关键字 (#106)

--- src/main/java/com/github/hcsp/inheritance/Base.java
+++ src/main/java/com/github/hcsp/inheritance/Base.java
@@ -1,7 +1,8 @@
 package com.github.hcsp.inheritance;
 
-public final class Base {
-    public final void sayHello() {
+public  class Base {
+
+    public  void sayHello() {
         System.out.println("我是父类！");
     }
 }
