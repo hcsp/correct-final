43d1b709ee3d1cdfddecb6b62fdc8b3fc6e2f008
wjxalexander <37684072+wjxalexander@users.noreply.github.com>
2022-08-20 21:59:34 +0800

    done (#243)
    done (#243)

--- src/main/java/com/github/hcsp/inheritance/Sub.java
+++ src/main/java/com/github/hcsp/inheritance/Sub.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.inheritance;
 
-public class Sub extends Base {
+public final class Sub extends Base {
     @Override
     public void sayHello() {
         System.out.println("我是子类！");
